e74ab9fcd69958d4116ddb3b2d7edb0144e8ab50
jhoanse98 <mansleobenitez@gmail.com>
2020-06-19 11:41:05 -0500

    Ajuste inicial de App y creación de formulario

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled';
+import criptomonedas from './cryptomonedas.png';
+
+import Formulario from './components/Formulario';
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+
+  @media (min-width: 992px){
+    display:grid;
+    grid-template-columns: repeat(2,1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Heading = styled.h1`
+  font-family: 'Bebas Neue', cursive;
+  color: #FFF;
+  text-align: left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 5px;
+    background-color: #66A2FE;
+    display: block;
+  };
+`;
+
+const Imagen = styled.img`
+  width: 100%;
+  margin-top: 5rem;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Contenedor>
+        <div>
+          <Imagen src={criptomonedas} alt="Criptomonedas" />  
+        </div>
+        <div>
+          <Heading>Criptomonedas</Heading>
+          <Formulario/>
+        </div>
+        
+      </Contenedor>
   );
 }
 
